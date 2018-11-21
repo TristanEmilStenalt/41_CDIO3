901bfb0765e4fa91ce4ae4030ebabf6d90fbecf6
Mark <markmalloy96@yahoo.dk>
2018-11-21 21:09:18 +0100

    test commit

--- src/Boundry/MonopolyGUI.java
+++ src/Boundry/MonopolyGUI.java
@@ -1,9 +1,13 @@
 package Boundry;
+import gui_codebehind.GUI_BoardController;
+import gui_fields.GUI_Player;
 import gui_main.GUI;
-
-public class MonopolyGUI {
+public class MonopolyGUI{
 
     GUI gui = new GUI();
 
-
+    public void test(){
+        // Tester ved at sætte dice til 3,4
+        gui.setDice(3,4);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import Boundry.MonopolyGUI;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        MonopolyGUI init = new MonopolyGUI();
+        init.test();
     }
 
 
