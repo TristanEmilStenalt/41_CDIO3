88db76c20bf3ca3929326564fb3a5273a39a93db
Tristan <tris2012@live.dk>
2018-11-21 09:15:58 +0100

    Added constructor.

--- src/Entites/PlayerList.java
+++ src/Entites/PlayerList.java
@@ -2,14 +2,22 @@ package Entites;
 
 public class PlayerList {
 
-    Player player1 = new Player("playerOne", 0,0);
-    Player player2 = new Player("playerTwo", 0,0);
-    Player player3 = new Player("playerThree", 0,0);
-    Player player4 = new Player("playerFour", 0,0);
-    Player[] thePlayers = new Player[4];
+    // Four fixed players in an array
+    private Player player1 = new Player("playerOne", 0,0);
+    private Player player2 = new Player("playerTwo", 0,0);
+    private Player player3 = new Player("playerThree", 0,0);
+    private Player player4 = new Player("playerFour", 0,0);
+    private Player[] thePlayers = {player1,player2,player3,player4};
+
+    public PlayerList (Player[] thePlayers) //Constructor
+    {
+        this.thePlayers = thePlayers;
+    }
 
 
-    public Player[] createPlayerList() {
+    public Player[] createPlayerList() //createPlayerList method, basically get method.
+    {
         return thePlayers;
     }
+
 }
