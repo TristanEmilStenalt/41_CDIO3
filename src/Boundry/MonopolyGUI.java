1808ab32b6b38267421dedf29b11b1cb1cc914a2
mcmalloy <xomagametv@gmail.com>
2018-11-27 11:46:16 +0100

    Merge branch 'GUI' of C:\Users\markm\Desktop\Workspace\41_CDIO3 with conflicts.

--- src/Boundry/MonopolyGUI.java
+++ src/Boundry/MonopolyGUI.java
@@ -1,15 +1,16 @@
 package Boundry;
-
+import gui_codebehind.GUI_BoardController;
+import gui_fields.GUI_Board;
 import gui_fields.GUI_Field;
 import gui_fields.GUI_Player;
 import gui_main.GUI;
 
-
+import java.awt.*;
+import Boundry.GUI_FieldFactory;
 public class  MonopolyGUI{
 
+
     GUI gui = new GUI();
-
-
 
     public void test(){
         // Tester ved at sætte dice til 3,4
@@ -17,11 +18,11 @@ public class  MonopolyGUI{
         fieldCreation();
     }
 
+    public void fieldCreation(){
+       // GUI_FieldFactory ini = new GUI_FieldFactory();
+        // ini.CreateFields();
 
-
-    public void fieldCreation() //forsøg på at bruge GUI. opsætning af bræt.
-    {
-        GUI_Field[] fields = GUI_FieldFactory.createFields();
+        GUI_Field[] fields = GUI_FieldFactory.CreateFields();
         GUI_Field[] fields2 = new GUI_Field[24];
 
         for(int i = 0; i < fields2.length; ++i) {
@@ -29,22 +30,17 @@ public class  MonopolyGUI{
         }
 
         GUI gui = new GUI(fields2);
-
-        GUI_Player gui_Player1 = new GUI_Player("player 1: Navn");
-        gui.addPlayer(gui_Player1);
-        GUI_Player gui_Player2 = new GUI_Player("player 2: Navn");
-        gui.addPlayer(gui_Player2);
-        GUI_Player gui_Player3 = new GUI_Player("player 3: Navn");
-        gui.addPlayer(gui_Player3);
-        GUI_Player gui_Player4 = new GUI_Player("player 4: Navn");
-        gui.addPlayer(gui_Player4);
-
-        fields[0].setCar(gui_Player1, true);
-        fields[6].setCar(gui_Player2, true);
-        fields[9].setCar(gui_Player3, true);
-        fields[17].setCar(gui_Player4, true);
+        System.out.println(gui.getUserButtonPressed("test\ntest", new String[]{"test1", "test2"}));
+        gui.getUserString("test\ntest");
+        gui.showMessage("test\ntest");
+        GUI_Player gui_Player = new GUI_Player("Brian123456789012345667");
+        gui.addPlayer(gui_Player);
+        gui.addPlayer(new GUI_Player("Brian123123213213213213"));
+        gui.addPlayer(new GUI_Player("Brian2"));
+        gui.addPlayer(new GUI_Player("Brian3"));
+        fields[23].setSubText("No Luck");
+        fields[0].setCar(gui_Player, true);
+        fields[0].setCar(gui_Player, false);
+        System.out.println(fields[0].hasCar(gui_Player));
     }
-
-
-
 }
