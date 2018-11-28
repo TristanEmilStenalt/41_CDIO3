3e13cad6eac0d55a15579edc9b35fe738d1784a9
Mark Malloy <markmalloy96@yahoo.dk>
2018-11-28 22:10:33 +0100

    now they roll around the board. OutOfBoundsException expected

--- src/Gamelogic/Game.java
+++ src/Gamelogic/Game.java
@@ -5,12 +5,14 @@ import java.util.*;
 
 public class Game extends MonopolyGUI {
     public int position;
-    public int rollDice;
+    public int roll;
     public int rollDice(){
         Random rand = new Random(); //Make an instance of Random
-        rollDice = rand.nextInt(6)+1;
-         return rollDice;
+        roll = rand.nextInt(6)+1;
+         return roll;
     }
+
+
     public int playerPositionArray[]={0,0,0,0};
     public void playerTurn() {
         boolean run = true;
@@ -21,13 +23,14 @@ public class Game extends MonopolyGUI {
                 // Prompts in the UI for the player to roll the dice.
                 MonopolyGUI rollText = new MonopolyGUI();
                 String rollTurn = gui.getUserString("Press ok to roll.");
-
-                playerPositionArray[i-1]=+rollDice();
+                rollDice();
+                playerPositionArray[i-1]+=roll;
                 //Now the player has rolled. Before he puts the car on the new field, the car is removed from the previous
                 fields[playerPositionArray[i-1]].removeAllCars();
                 System.out.println("Player " + i + " rolled: " + playerPositionArray[i-1]); // used for debugging in console
-                System.out.println(rollDice);
-                fields[playerPositionArray[i-1]].setCar(playerList[i - 1], true); // This sets the new position of the car.
+                System.out.println(roll);
+                fields[playerPositionArray[i-1]].setCar(playerList[i - 1],true); // This sets the new position of the car.
+                gui.showMessage("You rolled "+roll);
                 // fields[]
             }
         }
