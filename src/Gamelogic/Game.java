a7f67b012ac0586d7e3dc4e513eebfaca584cfa2
Mark Malloy <markmalloy96@yahoo.dk>
2018-11-28 22:43:06 +0100

    Now the players can move around the board unhindered

--- src/Gamelogic/Game.java
+++ src/Gamelogic/Game.java
@@ -24,13 +24,15 @@ public class Game extends MonopolyGUI {
                 MonopolyGUI rollText = new MonopolyGUI();
                 String rollTurn = gui.getUserString("Press ok to roll.");
                 rollDice();
-                playerPositionArray[i-1]+=roll;
+
                 //Now the player has rolled. Before he puts the car on the new field, the car is removed from the previous
                 fields[playerPositionArray[i-1]].removeAllCars();
-                System.out.println("Player " + i + " rolled: " + playerPositionArray[i-1]); // used for debugging in console
+                playerPositionArray[i-1]+=roll;
+                playerPositionArray[i-1]=playerPositionArray[i-1]%fields.length;
+                System.out.println("Player " + i + " rolled: " + roll); // used for debugging in console
                 System.out.println(roll);
-                fields[playerPositionArray[i-1]].setCar(playerList[i - 1],true); // This sets the new position of the car.
-                gui.showMessage("You rolled "+roll);
+                fields[playerPositionArray[i-1]].setCar(playerList[i-1],true); // This sets the new position of the car.
+                gui.showMessage("You rolled "+roll+" and landed on "+playerPositionArray[i-1]);
                 // fields[]
             }
         }
